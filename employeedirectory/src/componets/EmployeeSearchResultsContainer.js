6259527495275c3fba8985d850477b98c8f1f73b
The Noise <leespaulding25@gmail.com>
2021-05-01 04:57:55 -0400

    added state for Emp Res Container

--- employeedirectory/src/componets/EmployeeSearchResultsContainer.js
+++ employeedirectory/src/componets/EmployeeSearchResultsContainer.js
@@ -1,4 +1,13 @@
-import React, { componets } from "react";
+import React, { Component, componets } from "react";
 import EmployeeSearchFrom from "./EmployeeSearchForm";
 import EmployeeSearchResultsContainer from "./EmployeeSearchResultsContainer";
 import API from "../utils/API";
+
+class EmployeeSearchResultsContainer extends Component {
+    state ={
+        sortAsc:true,
+        employees:[],
+        search: "",
+        filteredemployees:[],
+    };
+}
