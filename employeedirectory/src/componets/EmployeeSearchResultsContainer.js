15651c0340f24c1ae2cd17ad3b81e8654beef418
The Noise <leespaulding25@gmail.com>
2021-05-01 04:47:12 -0400

    imported files to results container

---
+++ employeedirectory/src/componets/EmployeeSearchResultsContainer.js
@@ -0,0 +1,4 @@
+import React, { componets } from "react";
+import EmployeeSearchFrom from "./EmployeeSearchForm";
+import EmployeeSearchResultsContainer from "./EmployeeSearchResultsContainer";
+import API from "../utils/API";
